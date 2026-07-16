0ae5d94060202f2fbdf80693d8612249daba9b95
venugopal.kulkarni <venugopal.kulkarni@gmail.com>
2026-07-16 20:22:28 +0530

    workflow changes aspnet

--- src/aspnetapp/Program.cs
+++ src/aspnetapp/Program.cs
@@ -3,7 +3,7 @@ using aspnetapp;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to test container.
 builder.Services.AddRazorPages();
 builder.Services.AddHealthChecks();
 
