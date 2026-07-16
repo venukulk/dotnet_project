8010072ead7a8a9dc128fd476724a45e31273b3a
venugopal.kulkarni <venugopal.kulkarni@gmail.com>
2026-07-16 17:25:36 +0530

    workfloe changes

--- src/aspnetapp/Program.cs
+++ src/aspnetapp/Program.cs
@@ -3,7 +3,7 @@ using aspnetapp;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to test the container.
+// Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddHealthChecks();
 
